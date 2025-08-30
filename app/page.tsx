85d4677ce0b20b8445d4cc5be64f8457017abd07
quddus larik <tg53rt3@gmail.com>
2025-08-30 12:11:12 +0500

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 "use client"
 export default function Page() {
-
-
-
+  
   return (
     <>
-    <div className="bg-slate-50">
-      <h1>hi go to dashboard</h1> 
-      <a href="/dashboard">Dashboard</a>
-    </div>
+      <div className="h-screen w-full flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center flex-col p-10 ring-1 ring-sky-500 rounded-sm">
+          <h1>hi! go to dashboard</h1>
+          <a href="/dashboard" className="text-sky-300">/dashboard</a>
+        </div>
+      </div>
     </>
   );
 }
